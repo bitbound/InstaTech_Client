b893ec546674588ba07546f71ebc3d9c0fb51883
Jared Goodwin <jared@translucency.info>
2016-11-03 18:47:05 -0700

    Check-in.  Unable to build RobotJS in new dev environment.

--- InstaTech CP/app/main.js
+++ InstaTech CP/app/main.js
@@ -4,7 +4,7 @@ const electron = require('electron');
 if (require('electron-squirrel-startup')) return;
 
 // Set to true to enable dev tools for debugging. (Note: Server target is in index.js)
-var debug = false;
+var debug = true;
 
 ///<reference path="typings/index.d.ts" />
 const app = electron.app;
